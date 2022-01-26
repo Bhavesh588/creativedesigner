1a2c054365f85dd0a0a2451c57e64861816e98a3
Bhavesh588 <34418394+Bhavesh588@users.noreply.github.com>
2022-01-26 19:43:01 +0530

    Coding box done

--- src/Pages/CodingPlatform/CodingPlatform.js
+++ src/Pages/CodingPlatform/CodingPlatform.js
@@ -1,28 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './CodingPlatform.scss'
 import Files from '../../Components/Files/Files';
 
 const CodingPlatform = () => {
+
+    const [html, setHtml] = useState('')
+    const [css, setCss] = useState('')
+    const [js, setJs] = useState('')
+
 	return (
 		<div className='codingplatform'>
             <div className='container-fluid'>
                 <div className='row p-5'>
                     {/* All Code Files Names */}
-                    <div className='col-lg'>
+                    <div className='col-lg scrollable'>
                         <div className='files_outer'>
-                            <Files filename="doing.html" />
+                            <Files filename="doing.html" value={html} onChange={setHtml} />
                         </div>
                         <div className='files_outer'>
-                            <Files filename="anything.css" />
+                            <Files filename="anything.css" value={css} onChange={setCss} />
                         </div>
                         <div className='files_outer'>
-                            <Files filename="like.js" />
+                            <Files filename="like.js" value={js} onChange={setJs} />
                         </div>
                     </div>
                     {/* Live Preview, Console, etc... */}
                     <div className='col-lg'>
-                        
                     </div>
                 </div>
             </div>
