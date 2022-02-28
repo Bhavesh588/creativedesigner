38667824dd3b91fb6a05c109ec7b9eb3e3675433
Bhavesh588 <34418394+Bhavesh588@users.noreply.github.com>
2022-02-28 11:40:58 +0530

    Live Preview

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,30 +1,52 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from 'react'
+import { connect } from 'react-redux'
 
 import './Button.scss'
 import variable from '../../responsive/_variables.scss'
 
 const Button = ({ children, ...props }) => {
-
     const { darkmode } = props
 
     return (
-        <div className='button'>
-            {
-                props.type === 'buttontxt'
-                ? props.name === 'Settings'
-                    ? <button className='button_btn_txt' style={{ color: darkmode ? variable.color2 : variable.color1, backgroundColor: darkmode ? variable.color3 : 'rgba(0,0,0,0.3)' }} onClick={() => props.setToggleDrop(!props.toggleDrop)}>{children}</button>
-                    : <button className='button_btn_txt' style={{ color: darkmode ? variable.color2 : variable.color1, backgroundColor: darkmode ? variable.color3 : 'rgba(0,0,0,0.3)' }}>{children}</button>
-                : <button className='button_btn_pic'>{children}</button>
-            }
+        <div className="button">
+            {props.type === 'buttontxt' ? (
+                props.name === 'Settings' ? (
+                    <button
+                        className="button_btn_txt"
+                        style={{
+                            color: darkmode ? variable.color2 : variable.color1,
+                            backgroundColor: darkmode
+                                ? variable.color3
+                                : 'rgba(0,0,0,0.3)',
+                        }}
+                        onClick={() => props.setToggleDrop(!props.toggleDrop)}
+                    >
+                        {children}
+                    </button>
+                ) : (
+                    <button
+                        className="button_btn_txt"
+                        style={{
+                            color: darkmode ? variable.color2 : variable.color1,
+                            backgroundColor: darkmode
+                                ? variable.color3
+                                : 'rgba(0,0,0,0.3)',
+                        }}
+                    >
+                        {children}
+                    </button>
+                )
+            ) : (
+                <button className="button_btn_pic">{children}</button>
+            )}
         </div>
-    );
-};
+    )
+}
 
 const mapStateToProps = (state) => {
-	return {
-		darkmode: state.darkmode,
-	};
-};
+    return {
+        darkmode: state.darkmode,
+    }
+}
 
-export default connect(mapStateToProps, null)(Button);
+export default connect(mapStateToProps, null)(Button)
--- src/Pages/CodingPlatform/CodingPlatform.js
+++ src/Pages/CodingPlatform/CodingPlatform.js
@@ -1,17 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 import './CodingPlatform.scss'
-import Files from '../../Components/Files/Files';
+import Files from '../../Components/Files/Files'
+import LivePreview from '../../Components/LivePreview/LivePreview'
 
 const CodingPlatform = () => {
+    const [filename, setFilename] = useState([
+        'doing.html',
+        'anything.css',
+        'like.js',
+    ])
+    const [code, setCode] = useState([
+        `
+<div class="luke">
+    <h1>Hello Lucifer</h1>
+    <h2>Experience is the name everyone gives to their mistakes</h2>
+<div>
+        `,
+        `
+.luke {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
 
-    const [filename, setFilename] = useState(['doing.html', 'anything.css', 'like.js'])
-    const [code, setCode] = useState(['', '', ''])
+h1 {
+    margin: 0;
+    font-size: 40px;
+}
+
+h2 {
+    margin: 0;
+    font-size: 25px;
+	text-align: center;
+}
+
+@media only screen and (max-width : 300px) {
+	h1 {
+		font-size: 25px;
+	}
+	h2 {
+		font-size: 15px;
+	}
+}
+        `,
+        '',
+    ])
 
     // const [html, setHtml] = useState('')
     // const [css, setCss] = useState('')
     // const [js, setJs] = useState('')
-
 
     const onChangeFilename = (values, id) => {
         let val = [...filename]
@@ -22,8 +61,8 @@ const CodingPlatform = () => {
     const onAddFilename = (values, id) => {
         let val = [...filename]
         let co = [...code]
-        val.splice(id+1, 0, values)
-        co.splice(id+1, 0, '')
+        val.splice(id + 1, 0, values)
+        co.splice(id + 1, 0, '')
         setFilename(val)
         setCode(co)
     }
@@ -37,35 +76,34 @@ const CodingPlatform = () => {
         setCode(co)
     }
 
-	return (
-		<div className='codingplatform'>
-            <div className='container-fluid'>
-                <div className='row p-5'>
+    return (
+        <div className="codingplatform">
+            <div className="container-fluid">
+                <div className="row p-5">
                     {/* All Code Files Names */}
-                    <div className='col-lg scrollable'>
-                        {
-                            filename?.map((file, index) => 
-                                <div className='files_outer' key={index}>
-                                    <Files 
-                                        filename={file} 
-                                        id={index} 
-                                        values={code} 
-                                        onChange={setCode} 
-                                        onChangeFilename={onChangeFilename}
-                                        onAddFilename={onAddFilename}
-                                        onDeleteFilename={onDeleteFilename}
-                                    />
-                                </div>
-                            )
-                        }
+                    <div className="col-lg scrollable">
+                        {filename?.map((file, index) => (
+                            <div className="files_outer" key={index}>
+                                <Files
+                                    filename={file}
+                                    id={index}
+                                    values={code}
+                                    onChange={setCode}
+                                    onChangeFilename={onChangeFilename}
+                                    onAddFilename={onAddFilename}
+                                    onDeleteFilename={onDeleteFilename}
+                                />
+                            </div>
+                        ))}
                     </div>
                     {/* Live Preview, Console, etc... */}
-                    <div className='col-lg'>
+                    <div className="col-lg">
+                        <LivePreview code={code} />
                     </div>
                 </div>
             </div>
-		</div>
-	);
-};
+        </div>
+    )
+}
 
-export default CodingPlatform;
+export default CodingPlatform
